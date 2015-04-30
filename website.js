9515ef5476e425b92ecb98cea1e3ac8e21546f56
Thomas Levine <_@thomaslevine.com>
2015-04-30 22:01:15 +0000

    -

--- website.js
+++ website.js
@@ -3,15 +3,17 @@ function log_website() $('#profile-modal-website > a').each(function() {
 
   // Easier to copy these server logs than the logs in the browser
   document.body.innerHTML += '<img src="http://dada.pink/?foundation_maps&href=' + href + '" />'
+
 })
 
 function click_detail() $('.cell-details > span.table-detail-icon').click()
-
+function click_close_modal() $('button.close').click()
 function click_next() $('#fm-view-list .pagination .next').click()
 
 function page() {
   click_detail()
   setTimeout(log_website, 5000)
+  setTimeout(click_close_modal, 9000)
   setTimeout(click_next, 10000)
   setTimeout(page, 15000)
 }
